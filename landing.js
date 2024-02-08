d6e65cd02248edd28f021f9417866dcbdf4bf86d
Connor Spendlove <Connor.Spendlove44@gmail.com>
2024-02-08 01:24:15 -0700

    fixed viewport issues and cleaned up landing.html

--- landing.js
+++ landing.js
@@ -1,23 +1,11 @@
-// Fetch data from the CheapShark API
-fetch('https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15')
-    .then(response => response.json())
-    .then(data => {
-        // Filter games based on Metacritic score
-        const highScoreGames = data.filter(game => game.metacriticScore >= 80);
+var Dealsbutton = document.getElementById("Dealsbtn")
+var Freebutton = document.getElementById("Freebtn")
 
-        // Log the high score games to the console
-        console.log(highScoreGames);
-
-        // Select a random game
-        const randomGame = highScoreGames[Math.floor(Math.random() * highScoreGames.length)];
-
-        // Get the thumbnail URL
-        const thumbnailUrl = randomGame.thumb;
-
-        // Get the image element
-        const imageElement = document.getElementById('game-image');
-
-        // Replace the placeholder image with the thumbnail
-        imageElement.src = thumbnailUrl;
-    })
-    .catch(error => console.error(error));
+// changes button class type depending on screen width to change styling through bulma
+    if (window.innerWidth < 550){
+        Dealsbutton.classList.remove("is-large")
+        Dealsbutton.classList.add("is-medium")
+        Freebutton.classList.remove("is-large")
+        Freebutton.classList.add("is-medium")
+    
+    }
